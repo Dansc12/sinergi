89abdda828c94e7cd1c8f8b5c87668b0eaf9e303
Dansc12 <danchristenson@dailyedgewellness.com>
2026-01-15 14:59:43 +0000

    Update Supabase types

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -237,6 +237,41 @@ export type Database = {
         }
         Relationships: []
       }
+      group_join_requests: {
+        Row: {
+          created_at: string
+          group_id: string
+          id: string
+          status: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          group_id: string
+          id?: string
+          status?: string
+          updated_at?: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          group_id?: string
+          id?: string
+          status?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "group_join_requests_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       group_members: {
         Row: {
           group_id: string
@@ -276,6 +311,7 @@ export type Database = {
           creator_id: string
           description: string | null
           id: string
+          is_system: boolean
           name: string
           updated_at: string
           visibility: string | null
@@ -286,6 +322,7 @@ export type Database = {
           creator_id: string
           description?: string | null
           id?: string
+          is_system?: boolean
           name: string
           updated_at?: string
           visibility?: string | null
@@ -296,6 +333,7 @@ export type Database = {
           creator_id?: string
           description?: string | null
           id?: string
+          is_system?: boolean
           name?: string
           updated_at?: string
           visibility?: string | null
@@ -916,6 +954,7 @@ export type Database = {
           created_at: string
           id: string
           log_date: string
+          notes: string | null
           updated_at: string
           user_id: string
           weight: number
@@ -924,6 +963,7 @@ export type Database = {
           created_at?: string
           id?: string
           log_date: string
+          notes?: string | null
           updated_at?: string
           user_id: string
           weight: number
@@ -932,6 +972,7 @@ export type Database = {
           created_at?: string
           id?: string
           log_date?: string
+          notes?: string | null
           updated_at?: string
           user_id?: string
           weight?: number
@@ -1021,6 +1062,7 @@ export type Database = {
           visibility: string
         }[]
       }
+      get_user_stats: { Args: { target_user_id: string }; Returns: Json }
     }
     Enums: {
       [_ in never]: never
